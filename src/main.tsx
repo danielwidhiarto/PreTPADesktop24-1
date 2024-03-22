2aea74c0055c4f51c5a3c9fead72d626f48c6a38
danielwidhiarto <danielwidhiarto@gmail.com>
2024-03-22 12:14:21 +0700

    add auth router home n login

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./routes/HomePage";
+import LoginPage from "./routes/LoginPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path: "/auth/login",
+    element: <LoginPage/>,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 );
